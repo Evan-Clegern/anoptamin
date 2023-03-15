721fde21e5c8f84770b4e9a14f61af87fcc3b0f3
Evan Clegern <evanclegern.work@gmail.com>
2023-03-14 19:48:13 -0600

    Made 'glact.cpp' for 'glact.hpp'
    Signed-off-by: Evan Clegern <evanclegern.work@gmail.com>

---
+++ source/glact.cpp
@@ -0,0 +1,35 @@
+/********!
+ * @file  glact.cpp
+ * 
+ * @author
+ * 	Evan Clegern <evanclegern.work@gmail.com>
+ * 
+ * @date
+ * 	14 March 2023
+ * 
+ * @brief
+ * 	Backend for "include/glact.hpp"
+ * 
+ * @copyright
+ * 	Copyright (C) 2023 Evan Clegern
+ * 
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License, as published by
+ * 	the Free Software Foundation, version 3 of the License.
+ * 
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ * 
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ ********/
+
+#inclide "glact.hpp"
+
+namespace Anoptamin { namespace Graphics {
+	
+	
+}} //End Anoptamin::Graphics
