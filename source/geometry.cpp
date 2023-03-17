d6772c9d1fdecc073844f4a3a0186f779195660f
Evan Clegern <evanclegern.work@gmail.com>
2023-03-16 22:12:38 -0600

    Began Geometry support
    Signed-off-by: Evan Clegern <evanclegern.work@gmail.com>

---
+++ source/geometry.cpp
@@ -0,0 +1,37 @@
+/********!
+ * @file  geometry.cpp
+ * 
+ * @author
+ * 	Evan Clegern <evanclegern.work@gmail.com>
+ * 
+ * @date
+ * 	16 March 2023
+ * 
+ * @brief
+ * 	Backend code for 'include/geometry.hpp'
+ * 
+ * @copyright
+ * 	Copyright (C) 2023 Evan Clegern
+ * 
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License, as published by
+ * 	the Free Software Foundation, version 3 of the License.
+ * 
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ * 
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ ********/
+
+
+#include "../include/geometry.hpp"
+
+
+namespace Anoptamin { namespace Geometry {
+	
+	
+}}; //End Anoptamin::Geometry
