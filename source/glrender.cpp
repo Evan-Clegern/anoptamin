db5568bc62f93ca1a6b4189683d8aa8ea0e881b1
Evan Clegern <evanclegern.work@gmail.com>
2023-03-19 21:07:41 -0600

    Laid out basis for the next-level rendering system
    Signed-off-by: Evan Clegern <evanclegern.work@gmail.com>

---
+++ source/glrender.cpp
@@ -0,0 +1,34 @@
+/********!
+ * @file  glrender.cpp
+ * 
+ * @author
+ * 	Evan Clegern <evanclegern.work@gmail.com>
+ * 
+ * @date
+ * 	19 March 2023
+ * 
+ * @brief
+ * 	Provides backend code for "include/glrender.hpp"
+ * 
+ * @copyright
+ * 	Copyright (C) 2023 Evan Clegern
+ * 
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License, as published by
+ * 	the Free Software Foundation, version 3 of the License.
+ * 
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ * 
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ ********/
+
+#include "../include/glrender.hpp"
+
+namespace Anoptamin { namespace Graphics {
+	
+}};
