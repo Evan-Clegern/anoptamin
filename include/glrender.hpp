e99625485663f889b2037d5c7fdef96a8f7d0a69
Evan Clegern <evanclegern.work@gmail.com>
2023-03-23 23:13:12 -0600

    Started Render Object structure
    Signed-off-by: Evan Clegern <evanclegern.work@gmail.com>

--- include/glrender.hpp
+++ include/glrender.hpp
@@ -8,8 +8,9 @@
  * 	19 March 2023
  * 
  * @brief
- * 	Provides a combination between standard geometry objects and the
- *	lower-level OpenGL implementation. Provides includes in:
+ * 	Provides a combination between simple geometry objects and the
+ *	lower-level OpenGL implementation. This primarily involves making
+ *	renderable primitives and renderable objects. Provides includes in:
  *		Anoptamin::Graphics
  * 
  * @copyright
@@ -37,6 +38,19 @@
 
 namespace Anoptamin { namespace Graphics {
 	
+	//! Wrapper object which is intended to store the points and various buffers needed to draw any given primitive set.
+	struct c_RenderObject {
+		//! Vertex Buffer Object. Stores vertexes (points) for rendering.
+		uint32_t GL_VBO;
+		//! Index Buffer Object. Stores the indexes in which OpenGL places the vertexes for drawing lines.
+		uint32_t GL_IBO;
+		//! Vertex Array Object. Stores vertex attributes, vertex data formatting, and links to other buffer objects.
+		uint32_t GL_VAO;
+		//! OpenGL-format vertex set.
+		std::vector<double> GL_Vertexes;
+		//! OpenGL-format index set.
+		std::vector<uint32_t> GL_Indexes;
+	};
 	
 }}; //End Anoptamin::Graphics
 
